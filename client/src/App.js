dbb3b8994771d574f7a34714d376422b3e618db4
Alejandro <alex.bautista.ramos.90@gmail.com>
2018-12-26 16:27:51 -0700

    Up to this point, the app is fully working with a docker-compose up, there is no need to rebuild the images now.

--- client/src/App.js
+++ client/src/App.js
@@ -13,6 +13,7 @@ class App extends Component {
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
             <h1 className="App-title">Fib Calculator version 2</h1>
+            <p>Insert a number, click on submit and then refresh the page to visualize your results.</p>
             <Link to="/">Home</Link>
             <Link to="/otherpage">Other Page</Link>
           </header>
